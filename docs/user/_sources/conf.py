9b4e63d0c563bb9b851cc15f22f378b2d5e1df1f
sgiavasis <sgiava77@gmail.com>
2018-01-30 00:23:20 -0500

    Bunch of updates for 1.0.3 release.

--- docs/user/_sources/conf.py
+++ docs/user/_sources/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'C-PAC'
-copyright = u'2017, C-PAC Team'
+copyright = u'2018, C-PAC Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

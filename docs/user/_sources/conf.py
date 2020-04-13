07411a577aba08c32d6ab7dddad147c5ca2cf8f4
Jon Clucas <jon@vasegurt.com>
2020-04-13 13:11:42 -0400

    :construction: WIP seealso

--- docs/user/_sources/conf.py
+++ docs/user/_sources/conf.py
@@ -33,9 +33,10 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax',
-              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
-              'sphinxcontrib.programoutput', 'exec', 'numpydoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.ifconfig',
+              'sphinx.ext.intersphinx', 'sphinx.ext.viewcode',
+              'sphinx.ext.mathjax', 'sphinxcontrib.programoutput', 'exec',
+              'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

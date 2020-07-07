23094f1f70449e4ad87315c7539f362f7c987108
Jon Clucas <jon@vasegurt.com>
2020-07-07 18:14:10 -0400

    :hammer: Abstract checking for latest version

--- scripts/is_latest.py
+++ scripts/is_latest.py
@@ -1,11 +1,11 @@
 import os
+import sys
 
 from version_list import sort_versions
 
-try:
-    versions = os.listdir('/build/docs')
-    with open('/build/build_version.txt') as bv:
-        build_version = bv.read().strip()
-    return(sort_versions(versions)[1] == build_version)
-except:
-    return(False)
+versions = os.listdir('/build/docs')
+with open('/build/build_version.txt') as bv:
+    build_version = bv.read().strip()
+if(sort_versions(versions)[1] == build_version):
+    sys.exit(0)
+sys.exit(1)

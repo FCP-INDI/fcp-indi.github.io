7a56bef173a8a7dca5b07b25dc577a10a5daf6dc
Jon Clucas <jon@vasegurt.com>
2020-07-07 17:52:36 -0400

    :truck: Relocate versions.txt

--- scripts/version_list.py
+++ scripts/version_list.py
@@ -6,7 +6,7 @@ def rewrite_versions():
     Collects built versions and outputs a text file that lists 'latest', 
     latest version number, 'develop', descending prior verions.
     """
-    versions_txt_path = '/build/docs/_sources/_static/versions.txt'
+    versions_txt_path = '/build/docs/versions.txt'
     with open(versions_txt_path, 'w') as vtp:
         vtp.write('\n'.join(sort_versions(os.listdir('/build/docs'))))
     
@@ -14,7 +14,9 @@ def rewrite_versions():
 def sort_versions(versions):
     v2 = versions.copy()
     v2.sort(reverse=True)
-    {v2.remove(v) for v in {'latest', 'develop', '_sources'} if v in v2}
+    {v2.remove(v) for v in {
+        'latest', 'develop', '_sources', 'versions.txt'
+    } if v in v2}
     if len(v2):
         if len(v2)<2:
             return(['latest', v2[0], 'develop'])

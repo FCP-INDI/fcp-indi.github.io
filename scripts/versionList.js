ebb8ac78cbbc6d64d54e1ed69c7b6b993954d8ef
Jon Clucas <jon@vasegurt.com>
2020-07-08 11:31:01 -0400

    :ambulance: Await dropdown function before building dropdown

--- scripts/versionList.js
+++ scripts/versionList.js
@@ -1,31 +1,49 @@
 const versionPattern = /(?<=.*fcp-indi\.github\..*\/docs\/)(.*)(?=\/.*)/;
 
+function createDropdown(here) {
+  let promisedDropdown = function(resolve, reject) {
+    fetch("https://fcp-indi.github.io/docs/versions.txt").then(response => response.text().then(version_list => {
+      const versions = version_list.split('\n');
+      let dropdownElement = document.createElement('select');
+      versions.forEach(version => {
+        let option = document.createElement('option');
+        option.text = version;
+        option.value = version;
+        dropdownElement.add(option);
+        let indexInString = here.search(versionPattern);
+        if (here.slice(indexInString, indexInString + version.length) === version) {
+          option.setAttribute('selected', 'selected');
+        }
+      });
+      resolve(dropdownElement);
+    }));
+  }
+  return new Promise(promisedDropdown);
+}
+
 function versionDropdown() {
   const here = window.location.href;
-  const dochome = "https://" + here.split('/').slice(2, 6).join('/'); // make this 5 for top-level domain
+  const dochome = "https://" + here.split('/').slice(2, 5).join('/');
   const navTitles = document.getElementsByClassName("nav-item-0");
-  const dropdown = createDropdown(here);
-  for (let item of navTitles) {
-    let newTitle = document.createElement("div");
-    let newTitlePrefix = document.createElement("a");
-    newTitlePrefix.setAttribute("href", dochome);
-    console.log(newTitle);
-    newTitlePrefix.appendChild(document.createTextNode("C-PAC "));
-    newTitle.appendChild(newTitlePrefix);
-    console.log(newTitle);
-    newTitle.appendChild(dropdown);
-    console.log(newTitle);
-    let newTitleSuffix = document.createElement("a");
-    newTitleSuffix.setAttribute("href", dochome);
-    newTitleSuffix.appendChild(document.createTextNode(" documentation"));
-    newTitle.appendChild(newTitleSuffix);
-    newTitle.appendChild(document.createTextNode(" »"));
-    console.log(newTitle);
-    item.innerHTML = newTitle.innerHTML;
-    item.addEventListener('change', (event) => {
-      redirectVersion(here, event.target.value);
-    });
-  }
+  createDropdown(here).then(dropdown => {
+    for (let item of navTitles) {
+      let newTitle = document.createElement("div");
+      let newTitlePrefix = document.createElement("a");
+      newTitlePrefix.setAttribute("href", dochome);
+      newTitlePrefix.appendChild(document.createTextNode("C-PAC "));
+      newTitle.appendChild(newTitlePrefix);
+      newTitle.appendChild(dropdown);
+      let newTitleSuffix = document.createElement("a");
+      newTitleSuffix.setAttribute("href", dochome);
+      newTitleSuffix.appendChild(document.createTextNode(" documentation"));
+      newTitle.appendChild(newTitleSuffix);
+      newTitle.appendChild(document.createTextNode(" »"));
+      item.innerHTML = newTitle.innerHTML;
+      item.addEventListener('change', (event) => {
+        redirectVersion(here, event.target.value);
+      });
+    }
+  });
 }
 
 
@@ -40,22 +58,5 @@ function redirectVersion(here, version) {
 }
 
 
-function createDropdown(here) {
-  fetch("https://fcp-indi.github.io/docs/versions.txt").then(response => response.text().then(version_list => {
-    const versions = version_list.split('\n');
-    let dropdownElement = document.createElement('select');
-    versions.forEach(version => {
-      let option = document.createElement('option');
-      option.text = version;
-      option.value = version;
-      dropdownElement.add(option);
-      let indexInString = here.search(versionPattern);
-      if (here.slice(indexInString, indexInString + version.length) === version) {
-        option.setAttribute('selected', 'selected');
-      }
-    });
-    return(dropdownElement);
-  }));
-}
 
 versionDropdown();

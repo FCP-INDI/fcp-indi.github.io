9d9dd7fed077d1be0f040e5969d1cb776ef804d6
ci-build <ci-build@fcp-indi.github.com>
2020-07-08 17:07:37 +0000

    :rocket: Update release notes from GH

--- docs/user/_static/documentation_options.js
+++ docs/user/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.2 Beta',
+    VERSION: '1.6.2.a Beta',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',

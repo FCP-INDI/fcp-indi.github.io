2b4059e53117b8c178a4ac75a2784c992b46e05e
Jon Clucas <jon@vasegurt.com>
2020-07-27 13:31:30 -0400

    :rocket: Look in /tmp/repo/docs for versions when deploying

--- scripts/is_latest.py
+++ scripts/is_latest.py
@@ -3,7 +3,9 @@ import sys
 
 from version_list import sort_versions
 
-versions = os.listdir('/build/docs')
+
+docs_dir = sys.argv[1] if len(sys.argv) > 1 else '/build/docs'
+versions = os.listdir(docs_dir)
 with open('/build/build_version.txt') as bv:
     build_version = bv.read().strip()
 if(sort_versions(versions)[1] == build_version):

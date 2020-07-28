2e6a36e2e2fbb1e502ac30dab83b7275d90346ca
ci-build <ci-build@fcp-indi.github.com>
2020-07-28 20:57:39 +0000

    fixup! :handshake: Merge branch 'source' of github.com:FCP-INDI/fcp-indi.github.com into source

--- scripts/version_list.py
+++ scripts/version_list.py
@@ -1,29 +1,31 @@
 import os
 import sys
 
+from copy import copy
+
 
 def rewrite_versions(docs_path='/build/docs'):
     """
-    Collects built versions and outputs a text file that lists 'latest', 
+    Collects built versions and outputs a text file that lists 'latest',
     latest version number, 'nightly', descending prior verions.
     """
-    versions_txt_path = f'{docs_path}/versions.txt'
+    versions_txt_path = os.path.join(docs_path, 'versions.txt')
     with open(versions_txt_path, 'w') as vtp:
         vtp.write('\n'.join(sort_versions(os.listdir(docs_path))))
-    
+
 
 def sort_versions(versions):
-    v2 = versions.copy()
+    v2 = copy(versions)
     v2.sort(reverse=True)
     {v2.remove(v) for v in {
         'latest', 'nightly', '_sources', 'versions.txt'
     } if v in v2}
     if len(v2):
-        if len(v2)<2:
+        if len(v2) < 2:
             return(['latest', v2[0], 'nightly'])
-        return(['latest', v2[0], 'nightly', *v2[1:]])
+        return(['latest', v2[0], 'nightly'] + v2[1:])
     return(['latest', 'nightly'])
-    
+
 
 if __name__ == '__main__':
     rewrite_versions(sys.argv[1]) if len(sys.argv) > 1 else rewrite_versions()

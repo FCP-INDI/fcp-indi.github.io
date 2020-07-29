35c6bb06fac51e082d7094ea2664c4126d37bbb0
ci-build <ci-build@fcp-indi.github.com>
2020-07-29 16:01:24 +0000

    :truck: Fix path to version list

--- scripts/link_to_set_version.py
+++ scripts/link_to_set_version.py
@@ -1,6 +1,4 @@
-"""Python 2 version
-
-This script takes a version string as a commandline argument and updates
+"""This script takes a version string as a commandline argument and updates
 the links in `docs/{version}` that point to code in the `master` branch of
 FCP-INDI/C-PAC to code in the tagged version.
 
@@ -12,20 +10,7 @@ python link_to_set_version.py $VERSION
 import os
 import sys
 
-from fnmatch import filter as fnfilter
-
-
-def glob(pattern, recursive=True):
-    split_pattern = pattern.split('/')
-    filepath = '/'.join(split_pattern[0:-1])
-    pattern = split_pattern[-1]
-    del split_pattern
-
-    matches = []
-    for root, dirnames, filenames in os.walk(filepath):
-        for filename in fnfilter(filenames, pattern):
-            matches.append(os.path.join(root, filename))
-    return matches
+from glob import glob
 
 
 def set_version(version):
@@ -34,7 +19,9 @@ def set_version(version):
         'FCP-INDI/C-PAC/blob/master',
         'FCP-INDI/C-PAC/master'
     ]
-    for filepath in glob(f'docs/{version}/**', recursive=True):
+    for filepath in glob('/'.join([
+        'docs', version, '**'
+    ]), recursive=True):
         if 'release_notes' not in filepath and os.path.isfile(filepath):
             try:
                 # Replace links to master branch with links to specified branch

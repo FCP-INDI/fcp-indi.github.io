fec4b83416db96a063b2b1709ad7728fdbe14e7d
ci-build <ci-build@fcp-indi.github.com>
2020-09-03 14:54:52 +0000

    🔒 Upgrade CircleCI image, Sphinx & jQuery (#236)

--- docs/nightly/_static/documentation_options.js
+++ docs/nightly/_static/documentation_options.js
@@ -1,10 +1,12 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.0 Beta',
+    VERSION: '1.7.1.dev Beta',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: false
 };

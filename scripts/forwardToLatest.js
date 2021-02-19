a0cfa9dfe922a51eddc23c4adeceaa00cdc89c22
Jon Clucas <jon.clucas@childmind.org>
2021-02-19 12:40:05 -0500

    :children_crossing: Redirect nonexistant versions to latest

--- scripts/forwardToLatest.js
+++ scripts/forwardToLatest.js
@@ -1,5 +1,6 @@
 const oldPattern = /.*fcp-indi\.github\..*\/docs(\/user|\/developer|\/$|$)/;
 const oldPatternDelim = /fcp-indi\.github\..*\/docs\//;
+const notLatestPattern = /.*fcp-indi\.github\..*\/docs(?!\/latest|\/$|$)/;
 const latestString = "/latest/";
 
 function constructLatest(loc) {
@@ -7,6 +8,10 @@ function constructLatest(loc) {
     const part0 = loc.match(oldPattern)[0].split('/').slice(0, -1).join('/');
     components = loc.split(oldPatternDelim);
     return(part0 + latestString + components[1]);
+  } else if (loc.search(notLatestPattern) !== -1) {
+    const part0 = loc.match(notLatestPattern)[0].split('/').slice(0, -1).join('/');
+    components = loc.split(oldPatternDelim);
+    return(part0 + latestString + components[1].split('/').slice(1,).join('/'));
   } else {
     return(loc);
   };

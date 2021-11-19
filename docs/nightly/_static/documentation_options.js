9ec244adf553a13119d950d1aa5fdf3218812c94
ci-build <ci-build@fcp-indi.github.io>
2021-11-19 19:14:44 +0000

    📝 Document changes from "Fix Memory Bugs" (FCP-INDI/C-PAC#1531) (#256)

--- docs/nightly/_static/documentation_options.js
+++ docs/nightly/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.2..dev Beta',
+    VERSION: '1.8.2.dev Beta',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

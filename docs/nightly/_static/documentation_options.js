89e0c8f083872bbb64dd98f2e86d0799b96fee55
ChildMindInstituteCNL <CMI_CPAC_Support@childmind.org>
2022-06-28 20:56:11 +0000

    📝 Describe RBC for rbc-options preconfig (#286)

--- docs/nightly/_static/documentation_options.js
+++ docs/nightly/_static/documentation_options.js
@@ -1,14 +1,12 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.4.dev Beta',
-    LANGUAGE: 'en',
+    VERSION: '1.8.4 Beta',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
-    SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: false,
+    NAVIGATION_WITH_KEYS: false
 };

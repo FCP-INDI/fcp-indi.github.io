a16ab1079e2c1d45c9bbb3fe703076e67ed28100
Jon Clucas <jon.clucas@childmind.org>
2023-03-03 16:26:30 -0500

    :loud_sound: Debug code-block endings

--- docs/_sources/conf.py
+++ docs/_sources/conf.py
@@ -611,6 +611,7 @@ def format_node_block_docstrings(lines: list) -> None:
         lines.insert(insert_at, ".. code-block:: Python")
         if insert_herald:
             lines.insert(insert_at, "Node Block:")
+        print('\n'.join([f'|{line}|' for line in lines]))
 
 
 def initialize_factory() -> None:
